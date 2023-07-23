f8640aba27a0709e7c2a219a98f2dc00ee4c5778
danilopolani <danilo.polani@gmail.com>
2023-07-23 20:33:52 +0200

    try both flowbite and alpine nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'media',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'pastel-green': 'rgb(183, 212, 218)',
+				'pastel-yellow': 'rgb(254, 227, 198)',
+				'pastel-red': 'rgb(243, 196, 205)',
+				'pastel-purple': 'rgb(200, 190, 253)',
+				'pastel-black': 'rgb(20, 21, 32)',
+			},
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin'),
+	],
 }
