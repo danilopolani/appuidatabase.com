ac40a21c861662f64eb672c414e65b64c78ab580
danilopolani <danilo.polani@gmail.com>
2023-07-30 19:47:53 +0200

    fix count screens

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,39 +1,15 @@
-import { CollectionEntry, getCollection } from 'astro:content';
-import * as R from 'remeda';
+import type { CollectionEntry } from 'astro:content';
 // @ts-ignore
 import path from 'node:path';
-import { Platform, AppCategory } from './enums';
-
-export async function getCategoryTeaser(category: AppCategory) {
-  // Get 4 random apps for the category
-  const data = R.pipe(
-    await getCollection('apps', ({ data }) => data.category === category),
-    R.shuffle(),
-    R.take(4),
-  );
-  
-  // Then collect all the screens for those given 4 apps
-  const screens = await getCollection('screens', (screen) => 
-    data.map((app) => app.id).includes(
-      // @ts-ignore
-      screen.id.split(path.sep).pop()
-    )
-  );
-
-  // Finally match the apps with their screens count
-  return data.map((app) => ({
-    ...app.data,
-    screensCount: screens.filter((screen) => screen.id.endsWith(`${path.sep}${app.id}`))[0]!.data.length,
-  }));
-}
+import { Platform } from './enums';
 
 export function fetchAppsScreensCount(apps: CollectionEntry<'apps'>[], screens: CollectionEntry<'screens'>[]) {
   return apps.map((app) => ({
     ...app.data,
     id: app.id,
-    screensCount: screens.filter((screen) =>
-      screen.id.endsWith(`${path.sep}${app.id}`)
-    )[0]!.data.length,
+    screensCount: screens
+    .filter((screen) => screen.id.endsWith(`${path.sep}${app.id}`))
+    .reduce((accumulator, currentValue) => accumulator + currentValue.data.length, 0),
   }));
 }
 
