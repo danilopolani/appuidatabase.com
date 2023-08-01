2e00ca79c42b0b39be68e7770ea8ad4b994766b7
danilopolani <danilo.polani@gmail.com>
2023-08-01 14:50:09 +0200

    cleanup dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import vue from "@astrojs/vue";
+import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
