8ce9e20f3212c284632ed82c4f9e8029876d8373
danilopolani <danilo.polani@gmail.com>
2023-08-03 19:23:54 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
-
 import alpinejs from "@astrojs/alpinejs";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.appuidatabase.com',
-  integrations: [tailwind(), vue(), alpinejs()],
+  integrations: [tailwind(), vue(), alpinejs(), sitemap()],
   experimental: {
     assets: true
   }
