48a3964f9a46246e03257d6015a754c889a3bb2d
danilopolani <danilo.polani@gmail.com>
2023-08-03 18:10:05 +0200

    fix mobile menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.appuidatabase.com',
-  integrations: [tailwind(), vue()],
+  integrations: [tailwind(), vue(), alpinejs()],
   experimental: {
     assets: true
   }
